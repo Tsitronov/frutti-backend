bf4977c2b6bdc75636b07bac212b04f306a72f94
evgenii <tsitronov2017@gmail.com>
2025-10-02 21:42:58 +0200

    oct2_8

--- server.js
+++ server.js
@@ -11,14 +11,12 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 
 // 👉 Фикс CORS: Multiple origins + preflight
 app.use(cors({
   origin: [
-    'http://localhost:3000',  // Dev frontend
     'https://frutti.vercel.app'  // Prod frontend
   ],
   credentials: true,
