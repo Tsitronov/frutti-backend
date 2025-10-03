9ed9fe7a078e5317f64c7214333e747addaefea0
evgenii <tsitronov2017@gmail.com>
2025-10-03 23:03:34 +0200

    oct3z

--- server.js
+++ server.js
@@ -15,7 +15,6 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
-const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 
